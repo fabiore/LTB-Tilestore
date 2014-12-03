0a33805ea61825c01975aa183d210a0a2350c9a9
Raymond <raymond@raycom.com>
2014-12-03 14:03:37 +0100

    moved from '$scope' ti using 'this'

--- app/modules/myStacks/myStacks.js
+++ app/modules/myStacks/myStacks.js
@@ -5,20 +5,22 @@ angular.module('LTBApp.mystacks', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {  
     $routeProvider.when('/my-stacks', {
         templateUrl:'modules/myStacks/my_stacks.html',
-        controller:'MyStacksController'
+        controller:'MyStacksController',
+        controllerAs: 'mySCtrl'
     });
 }])
 
-.controller('MyStacksController', ['callApi', '$scope', '$http', '$location', function(callApi, $scope, $http, $location) {
+.controller('MyStacksController', ['callApi', '$http', '$location', function(callApi, $http, $location) {
     //@todo get stacks from API...                
-    //callApi.getStacks();
+   // callApi.getStacks();
     
-    $scope.myStacks={};
+    this.myStacks={};
+    var mStackcrtl = this;
     $http.get('data/data-results-a.json').success (function(data){
-            $scope.myStacks=data;
+            mStackcrtl.myStacks=data;
     });
     
-    $scope.openStack= function(stackid){
+    this.openStack= function(stackid){
         $location.path("/stack_edit/"+stackid);
     };
 }])
